25039537cc3119666a27aa8d0f8518709ca05116
1amol2 <amolchandra2006@gmail.com>
2022-04-23 17:12:20 +0530

    Updated db key of user stories with their respective UIDs

--- app/src/main/java/com/amol/realapp/chatty/activity/MainActivity.java
+++ app/src/main/java/com/amol/realapp/chatty/activity/MainActivity.java
@@ -41,152 +41,151 @@ import android.content.Context;
 import android.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
-    private MaterialToolbar tBar;
-     private BottomNavigationView btmNavView;
-    private FragmentManager fragmentManager;
-    private static final int GET_STATUS_IMAGE=60;
+  private MaterialToolbar tBar;
+  private BottomNavigationView btmNavView;
+  private FragmentManager fragmentManager;
+  private static final int GET_STATUS_IMAGE = 60;
+
+  private userProfile user;
+
+  private String key;
+  private SharedPreferences sPref;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    init();
+    listener();
     
-    private userProfile user;
-   
-    private String key;
-    private SharedPreferences sPref;
-        @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-      
-        init();
-        listener();
-        
-    }
+    
+  }
 
-    private void listener() {
-         
-     btmNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+  private void listener() {
 
-                @Override
-                public boolean onNavigationItemSelected(MenuItem p1) {
-                    Fragment fragment=null;
-                    switch(p1.getItemId()){
-                    case R.id.chats:
-                            fragment = new ChatsFragment();
-                            break;
-                    case R.id.groups:
-                            
-                        
-                         fragment=new GroupsFragment();
-                          
-                            
-                        break;
-                    case R.id.status:
-                         fragment=new ChatsFragment();
-                        Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
-                        intent.setType("image/*");
-                        startActivityForResult(intent,GET_STATUS_IMAGE);
-                        break;
-                        default:
-                        fragment=new ChatsFragment();
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
-                
-                    return true;
-                }
+    btmNavView.setOnNavigationItemSelectedListener(
+        new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        
-    });
+          @Override
+          public boolean onNavigationItemSelected(MenuItem p1) {
+            Fragment fragment = null;
+            switch (p1.getItemId()) {
+              case R.id.chats:
+                fragment = new ChatsFragment();
+                break;
+              case R.id.groups:
+                fragment = new GroupsFragment();
+
+                break;
+              case R.id.status:
+                fragment = new ChatsFragment();
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, GET_STATUS_IMAGE);
+                break;
+              default:
+                fragment = new ChatsFragment();
+            }
+            getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+
+            return true;
+          }
+        });
     btmNavView.setSelectedItemId(R.id.chats);
-        FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getUid()).addValueEventListener(new ValueEventListener(){
+    FirebaseDatabase.getInstance()
+        .getReference()
+        .child("Users")
+        .child(FirebaseAuth.getInstance().getUid())
+        .addValueEventListener(
+            new ValueEventListener() {
 
-                @Override
-                public void onDataChange(DataSnapshot p1) {
-                  if(p1.exists()){
-                            user=p1.getValue(userProfile.class); 
-                         } 
-                    
-        
-                    }
-                @Override
-                public void onCancelled(DatabaseError p1) {
+              @Override
+              public void onDataChange(DataSnapshot p1) {
+                if (p1.exists()) {
+                  user = p1.getValue(userProfile.class);
                 }
-                
-               });
-                
+              }
 
-                
-    }
-
-    private void init() {
-        tBar = findViewById(R.id.toolbar);
-        setSupportActionBar(tBar);
-        fragmentManager=getSupportFragmentManager();
-         btmNavView = findViewById(R.id.main_bottomNavigationView);
-        
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==GET_STATUS_IMAGE){
+              @Override
+              public void onCancelled(DatabaseError p1) {}
+            });
             
-        if(data!=null){
-         if(data.getData()!=null){
-             final Date date=new Date();
-        final StorageReference sRef=  FirebaseStorage.getInstance().getReference().child("status").child(date.getTime()+"");
-             sRef.putFile(data.getData()).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>(){
-
-                     @Override
-                     public void onComplete(Task<UploadTask.TaskSnapshot> p1) {
-                         sRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>(){
-
-                                 @Override
-                                 public void onSuccess(Uri p1) {
-                                  userStatus uStatus=new userStatus();
-                                   
-                                                
-                                    uStatus.setName(user.getName());
-                                   uStatus.setProfileImage(user.getUserProfileImage());
-                                   uStatus.setLastUpdated(date.getTime());
-                                   
-                                         
-                                 
-                                 HashMap<String,Object> obj=new HashMap<>();
-                                 obj.put("name",uStatus.getName());
-                                 obj.put("profileImage",uStatus.getProfileImage()); 
-                                 obj.put("lastUpdated",uStatus.getLastUpdated());
-                                 
-                                 String imageUrl=p1.toString();
-                                 Status status=new Status(imageUrl,uStatus.getLastUpdated());
-                                 String randomKey=FirebaseDatabase.getInstance().getReference().push().getKey();
-                                 FirebaseDatabase.getInstance().getReference()
-                                 .child("Stories")
-                                 .child(randomKey)
-                                 .updateChildren(obj);
-                                 
-                                 FirebaseDatabase.getInstance().getReference()
-                                 .child("Stories")
-                                 .child(randomKey)
-                                 .child("statuses")
-                                 .push()
-                                 .setValue(status);
-                                 }
-                                 
-
-                         
-                     });
-                     }
-
-             
-         });
-         }
-        }
             
-        }
-    }
+  }
 
-   
-    
-    
-   
-   
+  private void init() {
+    tBar = findViewById(R.id.toolbar);
+    setSupportActionBar(tBar);
+    fragmentManager = getSupportFragmentManager();
+    btmNavView = findViewById(R.id.main_bottomNavigationView);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == GET_STATUS_IMAGE) {
+
+      if (data != null) {
+        if (data.getData() != null) {
+          final Date date = new Date();
+          final StorageReference sRef =
+              FirebaseStorage.getInstance()
+                  .getReference()
+                  .child("status")
+                  .child(date.getTime() + "");
+          sRef.putFile(data.getData())
+              .addOnCompleteListener(
+                  new OnCompleteListener<UploadTask.TaskSnapshot>() {
+
+                    @Override
+                    public void onComplete(Task<UploadTask.TaskSnapshot> p1) {
+                      sRef.getDownloadUrl()
+                          .addOnSuccessListener(
+                              new OnSuccessListener<Uri>() {
+
+                                @Override
+                                public void onSuccess(Uri p1) {
+                                  userStatus uStatus = new userStatus();
+
+                                  uStatus.setName(user.getName());
+                                  uStatus.setProfileImage(user.getUserProfileImage());
+                                  uStatus.setLastUpdated(date.getTime());
+
+                                  HashMap<String, Object> obj = new HashMap<>();
+                                  obj.put("name", uStatus.getName());
+                                  obj.put("profileImage", uStatus.getProfileImage());
+                                  obj.put("lastUpdated", uStatus.getLastUpdated());
+
+                                  String imageUrl = p1.toString();
+                                  Status status = new Status(imageUrl, uStatus.getLastUpdated());
+                                  
+                                  FirebaseDatabase.getInstance()
+                                      .getReference()
+                                      .child("Stories")
+                                      .child(FirebaseAuth.getInstance().getUid())
+                                      .updateChildren(obj);
+
+                                  FirebaseDatabase.getInstance()
+                                      .getReference()
+                                      .child("Stories")
+                                      .child(FirebaseAuth.getInstance().getUid())
+                                      .child("statuses")
+                                      .push()
+                                      .setValue(status);
+                                }
+                              });
+                    }
+                  });
+        }
+      }
+    }
+  }
 }
+
+
+
+
