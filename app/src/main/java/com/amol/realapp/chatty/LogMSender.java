08f10a83897a8f09af7494b0f378a2c2c7abbf5d
1amol2 <amolchandra2006@gmail.com>
2022-05-13 17:58:08 +0530

    Formatted the code

--- app/src/main/java/com/amol/realapp/chatty/LogMSender.java
+++ app/src/main/java/com/amol/realapp/chatty/LogMSender.java
@@ -1,12 +1,12 @@
 package com.amol.realapp.chatty;
+
 import android.content.Context;
 import com.itsaky.androidide.logsender.LogSender;
 
-public class LogMSender{
-    
-    public void loggingStart(Context ctx){
-        
-      LogSender.startLogging(ctx);
-        
-     }
+public class LogMSender {
+
+  public void loggingStart(Context ctx) {
+
+    LogSender.startLogging(ctx);
+  }
 }
