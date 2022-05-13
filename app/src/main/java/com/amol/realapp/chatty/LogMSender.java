7df9379de17e7022182b12963e994ff7b1911282
1amol2 <amolchandra2006@gmail.com>
2022-05-13 17:23:58 +0530

    Added new class for sending logs

---
+++ app/src/main/java/com/amol/realapp/chatty/LogMSender.java
@@ -0,0 +1,12 @@
+package com.amol.realapp.chatty;
+import android.content.Context;
+import com.itsaky.androidide.logsender.LogSender;
+
+public class LogMSender{
+    
+    public void loggingStart(Context ctx){
+        
+      LogSender.startLogging(ctx);
+        
+     }
+}
